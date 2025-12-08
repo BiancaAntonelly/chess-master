ed389b6df34f1c0f4444d5f9451fd6251e0c7baa
Humberto Vitalino <humbertovitalino1@gmail.com>
2025-12-08 01:40:50 -0300

    teste

--- src/main/java/chess/ChessException.java
+++ src/main/java/chess/ChessException.java
@@ -2,18 +2,12 @@ package chess;
 
 import boardgame.BoardException;
 
-/**
- * Exceção específica para erros de xadrez.
- * Lançada quando ocorre uma violação de regras do jogo de xadrez.
- */
 public class ChessException extends BoardException {
 
     /*@ public normal_behavior
       @   requires message != null;
-      @   ensures getMessage().equals(message);
-      @   assignable \nothing;
       @*/
-    public ChessException(/*@ non_null @*/ String message) {
+    public ChessException(String message) {
         super(message);
     }
 }
